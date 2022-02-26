3822a3127146c3e00d84e6731904eedac0b882b2
Rishabh Sinha <67827763+ris-2107@users.noreply.github.com>
2022-02-26 22:12:52 +0530

    Update [...nextauth].js

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,8 +1,7 @@
 import NextAuth from "next-auth"
 import SpotifyProvider from "next-auth/providers/spotify"
 import spotifyApi, { LOGIN_URL } from "../../../lib/spotify" 
-import Login from "../../Login"
-
+import Login from "../../login"
 async function refreshAccessToken(token){
   
   try{
